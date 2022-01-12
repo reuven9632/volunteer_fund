abf5c5149a9ee257865c3d3280addb15e3d8bca9
reuven9632 <reuven9632@gmail.com>
2022-01-12 21:55:57 +0200

    add files

---
+++ src/main/java/com/deeds/volunteer_fund/VolunteerFundApplication.java
@@ -0,0 +1,13 @@
+package com.deeds.volunteer_fund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerFundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteerFundApplication.class, args);
+	}
+
+}
